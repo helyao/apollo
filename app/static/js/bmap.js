1beec6af7c2845471fe595e7c3b7d3ab87abc91d
helyao <helyao@qq.com>
2017-03-17 19:47:16 +0800

    add demo.html

--- app/static/js/bmap.js
+++ app/static/js/bmap.js
@@ -16,30 +16,31 @@ function setMapEvent(){
   map.enableDoubleClickZoom()
 }
 function addClickHandler(target,window){
-  target.addEventListener("click",function(){
+  target.addEventListener("click", function(){
     target.openInfoWindow(window);
   });
 }
+
 function addMapOverlay(){
-  var markers = [
-    {content:"北京市海淀区学院路35号世宁大厦902",title:"公司位置",imageOffset: {width:0,height:3},position:{lat:39.991751,lng:116.358822}}
-  ];
-  for(var index = 0; index < markers.length; index++ ){
-    var point = new BMap.Point(markers[index].position.lng,markers[index].position.lat);
-    var marker = new BMap.Marker(point,{icon:new BMap.Icon("http://api.map.baidu.com/lbsapi/createmap/images/icon.png",new BMap.Size(20,25),{
-      imageOffset: new BMap.Size(markers[index].imageOffset.width,markers[index].imageOffset.height)
-    })});
-    var label = new BMap.Label(markers[index].title,{offset: new BMap.Size(25,5)});
-    var opts = {
-      width: 200,
-      title: markers[index].title,
-      enableMessage: false
-    };
-    var infoWindow = new BMap.InfoWindow(markers[index].content,opts);
-    marker.setLabel(label);
-    addClickHandler(marker,infoWindow);
-    map.addOverlay(marker);
-  }
+  var addr = {
+    content: "北京市海淀区学院路35号世宁大厦902",
+    title: "公司位置",
+    imageOffset: {width:0, height:3},
+    position: {lat:39.991751, lng:116.358822}
+  };
+  var point = new BMap.Point(addr.position.lng, addr.position.lat);
+  var marker = new BMap.Marker(point,{icon:new BMap.Icon("http://api.map.baidu.com/lbsapi/createmap/images/icon.png",new BMap.Size(20,25),{
+    imageOffset: new BMap.Size(addr.imageOffset.width, addr.imageOffset.height)
+  })});
+  var opts = {
+    width: 200,
+    title: addr.title,
+    enableMessage: false
+  };
+  var infoWindow = new BMap.InfoWindow(addr.content, opts);
+  addClickHandler(marker,infoWindow);
+  map.addOverlay(marker);
+  marker.openInfoWindow(infoWindow);
 }
 //向地图添加控件
 function addMapControl(){
